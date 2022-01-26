ce39905ed0424f2ad9edb4cb9def0a71a919b761
vrossi96 <vrossi396@yahoo.it>
2022-01-26 15:25:12 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("Vue Ok", Vue);
+
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
