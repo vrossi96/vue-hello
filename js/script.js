67215cf8db91b862b40c7a24f77753fb3fbb9634
vrossi96 <vrossi396@yahoo.it>
2022-01-26 15:40:32 +0100

    add name in page with vue

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
  */
+
+const app = new Vue({
+   el: "#root",
+   data: {
+      userName: "",
+      userPic: "",
+   },
+});
