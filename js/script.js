e035fd9e3c01063a1fae83f98f093ad7439ae40e
vrossi96 <vrossi396@yahoo.it>
2022-01-27 14:32:05 +0100

    min fix

--- js/script.js
+++ js/script.js
@@ -16,18 +16,12 @@ const app = new Vue({
    methods: {
       normalPic() {
          if (!this.userPic) {
-            this.userPic = "https://i.kym-cdn.com/entries/icons/original/000/037/347/coverfight.jpg";
+            this.userPic = "https://curiosoggi.it/wp-content/uploads/2021/10/Chi-e-Hasbulla-mini-Khabib.jpg";
             console.log("CIAO");
          }
       },
    },
+   created() {
+      setInterval(this.normalPic, 9000);
+   },
 });
-
-normalPic = () => {
-   if (!app.userPic) {
-      app.userPic = "https://curiosoggi.it/wp-content/uploads/2021/10/Chi-e-Hasbulla-mini-Khabib.jpg";
-      console.log("CIAO");
-   }
-};
-
-setInterval(normalPic, 9000);
